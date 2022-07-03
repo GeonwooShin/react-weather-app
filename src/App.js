f37f6c74b628d1d642cc6688dda15f1f2162b153
GeonwooShin <rjsdnslawkd@naver.com>
2022-07-03 19:55:14 +0900

    컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import WeatherBox from "./components/WeatherBox";
+import WeatherBtn from "./components/WeatherBtn";
+
+const APIkey = "0a35e22940484a0f9ba359de5fa6b224";
 
 function App() {
+  useEffect(() => {
+    getCurLoc();
+  }, []);
+
+  const getCurLoc = () => {
+    console.log("현재위치 가져옴");
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurLoc(lat, lon);
+    });
+  };
+
+  const getWeatherByCurLoc = async (lat, lon) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <WeatherBox />
+        <WeatherBtn />
+      </div>
     </div>
   );
 }
---
+++ src/components/WeatherBox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WeatherBox = () => {
+  return (
+    <div>
+      <div>서울</div>
+      <h2></h2>
+    </div>
+  );
+};
+
+export default WeatherBox;
